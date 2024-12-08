bbd29143090a717957f8a340bcab4880813f8b73
Mgoodno1 <90008902+Mgoodno1@users.noreply.github.com>
2024-12-07 18:16:04 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,31 @@
 function mergesort(array) {
+    if (array.length <= 1) return array;
+
+    for (let size = 1; size < array.length; size *= 2) {
+        for (let start = 0; start < array.length; start += 2 * size) {
+            const mid = Math.min(start + size, array.length);
+            const end = Math.min(start + 2 * size, array.length);
+            merge(array, start, mid, end);
+        }
+    }
     return array;
+}
+
+function merge(array, start, mid, end) {
+    let left = start;
+    let right = mid;
+    
+    while (left < right && right < end) {
+        if (array[left] <= array[right]) {
+            left++;
+        } else {
+            let temp = array[right];
+            for (let i = right; i > left; i--) {
+                array[i] = array[i - 1];
+            }
+            array[left] = temp;
+            left++;
+            right++;
+        }
+    }
 }
